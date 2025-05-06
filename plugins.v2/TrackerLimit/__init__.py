680801841469f6db0fbd1e0979227ee7314f0332
fhj <f_mailbox@163.com>
2025-05-06 21:37:45 +0800

    更新作者

--- plugins.v2/TrackerLimit/__init__.py
+++ plugins.v2/TrackerLimit/__init__.py
@@ -44,7 +44,7 @@ class TrackerLimit(metaclass=ABCMeta):
     # 插件版本
     plugin_version = "0.0.1"
     # 插件作者
-    plugin_author = "jxxghp,Fone-1"
+    plugin_author = "jxxghp,sushidg"
     # 作者主页
     author_url = "https://github.com/Fone-1"
     # 插件配置项ID前缀
